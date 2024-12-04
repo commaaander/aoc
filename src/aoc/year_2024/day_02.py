c5d70cd1b25599edbbb0414cdd7a73612cf133e1
Robert Frießleben <robert.friessleben@shd-online.de>
2024-12-04 08:46:45 +0100

    feat: add day 2 solution part one, data and documentation

---
+++ src/aoc/year_2024/day_02.py
@@ -0,0 +1,69 @@
+"""https://adventofcode.com/2024/day/2"""
+
+from typing import Any, Dict, List, Tuple
+
+from adventofcode import LOG
+
+questions: List[str] = [
+    "",  # noqa: E501
+    "",  # noqa: E501
+]
+
+
+def part_one(raw_data: str, **kwargs: Dict[str, Any]) -> Tuple[str, int]:
+    LOG.debug(f"part_one({raw_data=}, {kwargs=})")
+    answer: int = 0
+    parsed_data = _parse_raw_data(raw_data)
+
+    answer = sum(1 for report in parsed_data if _report_is_safe(report))
+
+    return questions[0], answer
+
+
+def part_two(raw_data: str, **kwargs: Dict[str, Any]) -> Tuple[str, int]:
+    LOG.debug(f"part_two({raw_data=}, {kwargs=})")
+    answer: int = 0
+    parsed_data = _parse_raw_data(raw_data)
+
+    answer = sum(1 for report in parsed_data if _report_is_safe(report=report, use_problem_dampener=True))
+
+    return questions[1], answer
+
+
+def _parse_raw_data(raw_data: str) -> List[List[int]]:
+    parsed_data: List[List[int]] = []
+    for report in raw_data.split("\n"):
+        parsed_data.append([int(level) for level in report.split()])
+    return parsed_data
+
+
+def _report_is_safe(report: List[int], use_problem_dampener: bool = False) -> bool:
+    bad_level_count: int = 0
+    is_bad_level: bool = False
+    deltas: List[int] = [0]
+    for idx in range(1, len(report)):
+        delta: int = report[idx] - report[idx - 1]
+        deltas.append(delta)
+        if delta == 0 or abs(delta) > 3:
+            is_bad_level = True
+        if idx > 2:
+            old_delta: int = report[idx - 1] - report[idx - 2]
+            if _sign(delta) != _sign(old_delta):
+                is_bad_level = True
+        if is_bad_level:
+            bad_level_count += 1
+
+    if use_problem_dampener:
+        bad_level_count -= 1
+    report_is_safe: bool = bool(bad_level_count < 1)
+    LOG.debug(f"{report=} with {deltas=} is {report_is_safe=}. {bad_level_count=}")
+    return report_is_safe
+
+
+def _sign(x: int) -> int:
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return 0
