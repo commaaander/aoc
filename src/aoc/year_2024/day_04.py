614332710935b4b34c7c1483abc64787ad880fc9
Robert Frießleben <robert.friessleben@shd-online.de>
2024-12-05 12:04:57 +0100

    refactor: simplify day 4 solution by removing numpy dependency and optimizing logic

--- src/aoc/year_2024/day_04.py
+++ src/aoc/year_2024/day_04.py
@@ -1,9 +1,6 @@
 """https://adventofcode.com/2024/day/4"""
 
-from doctest import debug
 from typing import Any, Dict, List, Tuple
-
-import numpy as np
 
 from adventofcode import LOG
 
@@ -17,39 +14,15 @@ def part_one(raw_data: str, **kwargs: Dict[str, Any]) -> Tuple[str, int]:
     LOG.debug(f"part_one({raw_data=}, {kwargs=})")
     answer: int = 0
 
-    word_search_array = _parse_raw_data(raw_data)
-    length, width = word_search_array.shape
-    word_found_array = np.zeros((length, width), dtype=int)
-    xmas_found: int = 0
-    deltas = {
-        "East": [("X", 0, 0), ("M", 0, 1), ("A", 0, 2), ("S", 0, 3)],
-        "West": [("X", 0, 0), ("M", 0, -1), ("A", 0, -2), ("S", 0, -3)],
-        "South": [("X", 0, 0), ("M", 1, 0), ("A", 2, 0), ("S", 3, 0)],
-        "North": [("X", 0, 0), ("M", -1, 0), ("A", -2, 0), ("S", -3, 0)],
-        "Southeast": [("X", 0, 0), ("M", 1, 1), ("A", 2, 2), ("S", 3, 3)],
-        "Southwest": [("X", 0, 0), ("M", 1, -1), ("A", 2, -2), ("S", 3, -3)],
-        "Northeast": [("X", 0, 0), ("M", -1, 1), ("A", -2, 2), ("S", -3, 3)],
-        "Northwest": [("X", 0, 0), ("M", -1, -1), ("A", -2, -2), ("S", -3, -3)],
-    }
+    array, length, width = _parse_raw_data(raw_data)
 
-    for y in range(length):
-        for x in range(width):
-            for direction in deltas.keys():
-                if _check_word_search_array(word_search_array, (y, x), deltas[direction]):
-                    _set_word_found_array_values(word_found_array, (y, x), deltas[direction])
-                    xmas_found += 1
-                    LOG.debug(f"XMAS nr. {xmas_found:2d} found @({x+1:3d},{y+1:3d}) in direction {direction}")
-                    pass
+    answer = sum(
+        int(_check(x, y, *direction, "XMAS", array))
+        for y in range(length)
+        for x in range(width)
+        for direction in [(1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1)]
+    )
 
-    result: str = ""
-    for y in range(length):
-        for x in range(width):
-            result += word_search_array[y, x] if word_found_array[y, x] == 1 else "."
-        result += "\n"
-
-    print(result)
-
-    answer = xmas_found
     return questions[0], answer
 
 
@@ -57,64 +30,31 @@ def part_two(raw_data: str, **kwargs: Dict[str, Any]) -> Tuple[str, int]:
     LOG.debug(f"part_two({raw_data=}, {kwargs=})")
     answer: int = 0
 
-    word_search_array = _parse_raw_data(raw_data)
-    length, width = word_search_array.shape
-    word_found_array = np.zeros((length, width), dtype=int)
-    xmas_found: int = 0
-    deltas = {
-        "masXmas": [("M", -1, -1), ("A", 0, 0), ("S", 1, 1), ("M", 1, -1), ("A", 0, 0), ("S", -1, 1)],
-        "samXmas": [("S", -1, -1), ("A", 0, 0), ("M", 1, 1), ("M", 1, -1), ("A", 0, 0), ("S", -1, 1)],
-        "masXsam": [("M", -1, -1), ("A", 0, 0), ("S", 1, 1), ("S", 1, -1), ("A", 0, 0), ("M", -1, 1)],
-        "samXsam": [("S", -1, -1), ("A", 0, 0), ("M", 1, 1), ("S", 1, -1), ("A", 0, 0), ("M", -1, 1)],
-    }
+    array, length, width = _parse_raw_data(raw_data)
 
-    for y in range(length):
-        for x in range(width):
-            for direction in deltas.keys():
-                if _check_word_search_array(word_search_array, (y, x), deltas[direction]):
-                    _set_word_found_array_values(word_found_array, (y, x), deltas[direction])
-                    xmas_found += 1
-                    LOG.debug(f"XMAS nr. {xmas_found:2d} found @({x+1:3d},{y+1:3d}) in direction {direction}")
-                    pass
+    answer = sum(
+        int(
+            (_check(x, y, 1, 1, "SAM", array) or _check(x, y, 1, 1, "MAS", array))
+            and (_check(x + 2, y, -1, 1, "SAM", array) or _check(x + 2, y, -1, 1, "MAS", array))
+        )
+        for y in range(length)
+        for x in range(width)
+    )
 
-    result: str = ""
-    for y in range(length):
-        for x in range(width):
-            result += word_search_array[y, x] if word_found_array[y, x] == 1 else "."
-        result += "\n"
-
-    print(result)
-
-    answer = xmas_found
     return questions[1], answer
 
 
 def _parse_raw_data(raw_data: str):
-    ret_val: List[List[str]] = []
+    return (
+        {(x, y): char for y, row in enumerate(raw_data.split("\n")) for x, char in enumerate(list(row))},  # array
+        len(raw_data.split("\n")),  # length
+        max([len(row) for row in raw_data.split("\n")]),  # width
+    )
 
-    for row in raw_data.split("\n"):
-        ret_val.append(list(row))
 
-    return np.array(ret_val)
-
-
-def _check_word_search_array(word_search_array, start, coords) -> bool:
-    y, x = start
-    length, width = word_search_array.shape
-
-    try:
-        for letter, dy, dx in coords:
-            if y + dy < 0 or y + dy >= length or x + dx < 0 or x + dx >= width:
-                return False
-            if word_search_array[y + dy, x + dx] != letter:
-                return False
-    except:  # noqa: E722
-        return False
+def _check(x, y, dx, dy, word, array) -> bool:
+    for i, char in enumerate(word):
+        if char != array.get((x + i * dx, y + i * dy), "."):
+            return False
+    LOG.debug(f"{word} found @({x:3d},{y:3d}) in direction ({dx:2d},{dy:2d})")
     return True
-
-
-def _set_word_found_array_values(word_found_array, start, coords):
-    y, x = start
-
-    for _, dy, dx in coords:
-        word_found_array[y + dy, x + dx] = 1
